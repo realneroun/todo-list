1b8103b7ad0854506f409278c680ccfd1895f273
Trong Hau <realneroun@gmail.com>
2024-08-19 17:16:18 +0700

    update +1

--- src/App.js
+++ src/App.js
@@ -10,11 +10,16 @@ class App extends Component {
     this.state = {
       tasks: [],
       isDisplayForm: false,
+      taskEditing: null,
+      filter:{
+        name :'',
+        status: -1
+      }
     };
   }
 
   componentDidMount() {
-    console.log("Component Will Mount");
+    // console.log("Component Will Mount");
     if (localStorage && localStorage.getItem("tasks")) {
       var tasks = JSON.parse(localStorage.getItem("tasks"));
       this.setState({
@@ -22,31 +27,6 @@ class App extends Component {
       });
     }
   }
-
-  onGenerateData = () => {
-    var tasks = [
-      {
-        id: this.generateID(),
-        name: "hoc lap trinh",
-        status: true,
-      },
-      {
-        id: this.generateID(),
-        name: "ngu",
-        status: false,
-      },
-      {
-        id: this.generateID(),
-        name: "choi",
-        status: true,
-      },
-    ];
-    console.log(tasks);
-    this.setState({
-      tasks: tasks,
-    });
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
 
   s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
@@ -59,21 +39,130 @@ class App extends Component {
   }
 
   onToggleForm = () => {
+    if (this.state.isDisplayForm && this.state.taskEditing !== null) {
+      this.setState({
+        isDisplayForm: true,
+        taskEditing: null,
+      });
+    } else {
+      this.setState({
+        isDisplayForm: !this.state.isDisplayForm,
+        taskEditing: null,
+      });
+    }
+  };
+
+  onCloseForm = () => {
     this.setState({
-      isDisplayForm: !this.state.isDisplayForm,
+      isDisplayForm: false,
+      taskEditing: null,
     });
   };
 
-  onCloseForm = () =>{
-    console.log("hehe")
+  onShowForm = () => {
     this.setState({
-      isDisplayForm: false,
+      isDisplayForm: true,
     });
+  };
+
+  onFilter = (filterName , filterStatus) => {
+    filterStatus = parseInt(filterStatus)
+    this.setState({
+      filter : {
+        name :filterName.toLowerCase(),
+        status : filterStatus
+      }
+    })
   }
 
+  onSubmit = (data) => {
+    var { tasks } = this.state;
+    if (data.id === "") {
+      data.id = this.generateID();
+      tasks.push(data);
+    } else {
+      var index = this.findIndex(data.id);
+      tasks[index] = data;
+    }
+
+    this.setState({
+      tasks: tasks,
+      taskEditing: null,
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  onUpdateStatus = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    if (index !== -1) {
+      tasks[index].status = !tasks[index].status;
+      this.setState({
+        tasks: tasks,
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+  };
+
+  findIndex = (id) => {
+    var { tasks } = this.state;
+    var result = -1;
+    tasks.forEach((task, index) => {
+      if (task.id === id) {
+        return (result = index);
+      }
+    });
+    return result;
+  };
+
+  onDelete = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    if (index !== -1) {
+      tasks.splice(index, 1);
+      this.setState({
+        tasks: tasks,
+      });
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+    this.onCloseForm();
+  };
+
+  onUpdate = (id) => {
+    var { tasks } = this.state;
+    var index = this.findIndex(id);
+    var taskEditing = tasks[index];
+    this.setState({
+      taskEditing: taskEditing,
+    });
+    this.onShowForm();
+  };
+
   render() {
-    var { tasks, isDisplayForm } = this.state;
-    var elmTaskForm = isDisplayForm ? <TaskForm onCloseForm={this.onCloseForm} /> : "";
+    var { tasks, isDisplayForm, taskEditing , filter } = this.state;
+    if(filter){
+      if(filter.name){
+       tasks=tasks.filter((task)=>{
+          return task.name.toLowerCase().indexOf(filter.name) !== -1
+        })
+      }
+        tasks = tasks.filter((task)=>{
+          if(filter.status === -1){
+            return task;
+          }else{
+            return task.status ===  (filter.status === 1 ? true : false) 
+          }
+        })
+    }
+    var elmTaskForm = isDisplayForm ? (
+      <TaskForm
+        onSubmit={this.onSubmit}
+        task={taskEditing}
+        onCloseForm={this.onCloseForm}
+      />
+    ) : (
+      ""
+    );
     return (
       <div className="container">
         <div className="text-center">
@@ -95,21 +184,23 @@ class App extends Component {
                 : "col-xs-12 col-sm-12 col-md-12 col-lg-12"
             }
           >
-            <button type="button" className="btn btn-primary" onClick={this.onToggleForm}>
-              <span className="fa fa-plus mr-5"  />
-              Thêm Công Việc
-            </button>
             <button
               type="button"
-              className="btn btn-danger"
-              onClick={this.onGenerateData}
+              className="btn btn-primary"
+              onClick={this.onToggleForm}
             >
               <span className="fa fa-plus mr-5" />
-              Generate Data
+              Thêm Công Việc
             </button>
             <Control />
             <div className="row mt-15">
-              <TaskList tasks={tasks} />
+              <TaskList
+                tasks={tasks}
+                onUpdateStatus={this.onUpdateStatus}
+                onUpdate={this.onUpdate}
+                onDelete={this.onDelete}
+                onFilter={this.onFilter}
+              />
             </div>
           </div>
         </div>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 
 class TaskItem extends Component {
+
+    onUpdateStatus =() => {
+        this.props.onUpdateStatus(this.props.task.id)
+    }
+    onDelete = () =>{
+        this.props.onDelete(this.props.task.id)
+    }
+    onUpdate = () =>{
+        this.props.onUpdate(this.props.task.id)
+    }
   render() {
     var { task, index } = this.props;
     return (
@@ -9,6 +19,7 @@ class TaskItem extends Component {
         <td>{task.name}</td>
         <td className="text-center">
           <span
+           onClick={this.onUpdateStatus}
             className={
               task.status === true
                 ? "label label-danger"
@@ -19,12 +30,12 @@ class TaskItem extends Component {
           </span>
         </td>
         <td className="text-center">
-          <button type="button" className="btn btn-warning">
+          <button type="button" className="btn btn-warning" onClick={this.onUpdate}>
             <span className="fa fa-pencil mr-5" />
             Sửa
           </button>
           &nbsp;
-          <button type="button" className="btn btn-danger">
+          <button type="button" onClick={this.onDelete} className="btn btn-danger">
             <span className="fa fa-trash mr-5" />
             Xóa
           </button>
